8caa6d4409f8c228a98ae398aa4c51b2f67a377f
Volodymyr <StuparenkoVladimir@gmail.com>
2025-06-06 13:43:44 +0300

    fix server.js

--- src/server.js
+++ src/server.js
@@ -1,51 +1,5 @@
 // src/server.js
 
-// import express from 'express';
-// import pino from 'pino-http';
-// import cors from 'cors';
-
-// import { getEnvVar } from './utils/getEnvVar.js';
-
-// const PORT = Number(getEnvVar('PORT', '3000'));
-
-// export const startServer = () => {
-//   const app = express();
-
-//   app.use(express.json());
-//   app.use(cors());
-
-//   app.use(
-//     pino({
-//       transport: {
-//         target: 'pino-pretty',
-//       },
-//     }),
-//   );
-
-//   app.get('/', (req, res) => {
-//     res.json({
-//       message: 'Hello world! How are you ?',
-//     });
-//   });
-
-//   app.use(/(.*)/, (req, res, next) => {
-//     res.status(404).json({
-//       message: 'Not found',
-//     });
-//   });
-
-//   app.use((err, req, res, next) => {
-//     res.status(500).json({
-//       message: 'Something went wrong',
-//       error: err.message,
-//     });
-//   });
-
-//   app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-//   });
-// };
-
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
