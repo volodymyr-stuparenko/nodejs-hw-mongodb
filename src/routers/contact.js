3b1c3084d82d3260f48a5064cd7939514ed045bf
Volodymyr <StuparenkoVladimir@gmail.com>
2025-06-13 13:30:38 +0300

    delete ctrlWrapper

--- src/routers/contact.js
+++ src/routers/contact.js
@@ -3,13 +3,11 @@ import {
   getContactsController,
   getContactByIdController,
 } from '../controllers/contacts.js';
-import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
 router.get('/contacts', getContactsController);
 
 router.get('/contacts/:contactId', getContactByIdController);
-ctrlWrapper(getContactByIdController);
 
 export default router;
