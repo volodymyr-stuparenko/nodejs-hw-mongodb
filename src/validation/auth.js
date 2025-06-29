41b85890be145131c77de88c11c40a9a7bbffe1a
Volodymyr <StuparenkoVladimir@gmail.com>
2025-06-29 17:38:23 +0300

    add validation auth

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -2,11 +2,11 @@ import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required,
-  password: Joi.string().required,
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
 });
 
 export const loginUserSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(6).required(),
 });
