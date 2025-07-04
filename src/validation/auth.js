41f83b27d4907011f0fc10b72d7f4bbf969c8b97
Volodymyr <StuparenkoVladimir@gmail.com>
2025-07-04 12:58:12 +0300

    add validation reset password

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -10,3 +10,7 @@ export const loginUserSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
+
+export const requestResetEmailSchema = Joi.object({
+  email: Joi.string().email().required(),
+});
