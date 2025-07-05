beb00886a936575a36c9e94e5f409d1660d307d0
Volodymyr <StuparenkoVladimir@gmail.com>
2025-07-05 13:52:26 +0300

    update validation password

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -14,3 +14,8 @@ export const loginUserSchema = Joi.object({
 export const requestResetEmailSchema = Joi.object({
   email: Joi.string().email().required(),
 });
+
+export const resetPasswordSchema = Joi.object({
+  token: Joi.string().required(),
+  password: Joi.string().required(),
+});
