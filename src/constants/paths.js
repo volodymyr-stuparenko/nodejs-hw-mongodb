8fca97b7c4f8b5270c4579538b0bc629913541f5
Volodymyr <StuparenkoVladimir@gmail.com>
2025-07-08 13:32:19 +0300

    add multer and uploadFiles

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,3 +1,5 @@
 import path from 'node:path';
 
 export const TEMPLATE_DIR = path.join(process.cwd(), 'templates');
+export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -28,6 +28,9 @@ const contactsSchema = new Schema(
       ref: 'users',
       required: true,
     },
+    photo: {
+      type: String,
+    },
   },
   {
     timestamps: true,
